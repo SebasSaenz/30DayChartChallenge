973ba14862a06ac2807ebaf324e74aa470543b79
Johan Sebastián Sáenz <53427119+SebasSaenz@users.noreply.github.com>
2024-04-02 13:27:17 +0200

    update readme

--- 2024/code/Day_1_part_to_whole.R
+++ 2024/code/Day_1_part_to_whole.R
@@ -8,13 +8,9 @@ library(emojifont)
 library(showtext)
 
 # Load Eurovision votes dataset ------------------------------------------------
-eurovision <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-17/eurovision-votes.csv')
+eurovision <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-17/eurovision-votes.csv")
 
 # load fonts and titles
-
-font_add_google("Henny Penny", "hennypenny")
-font_add_google("Roboto", "roboto")
-showtext_auto()
 
 logo_txt <- "white"
 
@@ -23,109 +19,139 @@ twitter <- glue("<span style='font-family:\"fontawesome-webfont\";color:{logo_tx
 github <- glue("<span style='font-family:\"fontawesome-webfont\";color:{logo_txt}'>{emojifont::fontawesome('fa-github')}</span>")
 
 caption <- str_wrap(glue(
-  "Data source: Eurovision •  @SaenzJohanS • Code: SebasSaenz/TidyTuesday"))
+  "Data source: Eurovision •  @SaenzJohanS • Code: SebasSaenz/TidyTuesday"
+))
 
 base_color <- c("#fd8d3c", "#49006a")
 
 # Wrangle data
-df <- eurovision %>% 
-  filter(semi_final =="f",
-         jury_or_televoting == "T") %>%
+df <- eurovision %>%
+  filter(
+    semi_final == "f",
+    jury_or_televoting == "T"
+  ) %>%
   group_by(from_country, to_country) %>%
-  summarise(sum_points = sum(points), .groups = "drop") %>% 
+  summarise(sum_points = sum(points), .groups = "drop") %>%
   mutate(voted = if_else(sum_points > 0, "YES", "NO"))
 
 
-germnany <- df %>% 
-  filter(to_country == "Germany",
-         from_country != "Germany") %>% 
+germnany <- df %>%
+  filter(
+    to_country == "Germany",
+    from_country != "Germany"
+  ) %>%
   count(voted)
 
-germany_semicircle <- parliament_data(election_data = germnany,
-                                 type = "semicircle", # Parliament type
-                                 parl_rows = 4,      # Number of rows of the parliament
-                                 party_seats = germnany$n)
+germany_semicircle <- parliament_data(
+  election_data = germnany,
+  type = "semicircle", # Parliament type
+  parl_rows = 4, # Number of rows of the parliament
+  party_seats = germnany$n
+)
 
 
 germany_plot <- ggplot(germany_semicircle, aes(x = x, y = y, colour = voted)) +
   geom_parliament_seats(size = 4) +
-  geom_text(x =0, y = 0.2, label = "Germany", color = "white", size=5, family = "Henny Penny") +
+  geom_text(x = 0, y = 0.2, label = "Germany", color = "white", size = 5, family = "Henny Penny") +
   theme_ggparliament(legend = "none") +
-  scale_color_manual(values = base_color) 
+  scale_color_manual(values = base_color)
 
 
-france <- df %>% 
-  filter(to_country == "France",
-         from_country != "France") %>% 
+france <- df %>%
+  filter(
+    to_country == "France",
+    from_country != "France"
+  ) %>%
   count(voted)
 
-france_semicircle <- parliament_data(election_data = france,
-                                      type = "semicircle", # Parliament type
-                                      parl_rows = 4,      # Number of rows of the parliament
-                                      party_seats = france$n)
+france_semicircle <- parliament_data(
+  election_data = france,
+  type = "semicircle", # Parliament type
+  parl_rows = 4, # Number of rows of the parliament
+  party_seats = france$n
+)
 
 
 france_plot <- ggplot(france_semicircle, aes(x = x, y = y, colour = voted)) +
   geom_parliament_seats(size = 4) +
-  geom_text(x =0, y = 0.2, label = "France", color = "white", size=5, family = "Henny Penny") +
+  geom_text(x = 0, y = 0.2, label = "France", color = "white", size = 5, family = "Henny Penny") +
   theme_ggparliament(legend = "none") +
   scale_color_manual(values = base_color)
 
 
-spain <- df %>% 
-  filter(to_country == "Spain",
-         from_country != "Spain") %>% 
+spain <- df %>%
+  filter(
+    to_country == "Spain",
+    from_country != "Spain"
+  ) %>%
   count(voted)
 
-spain_semicircle <- parliament_data(election_data = spain,
-                                     type = "semicircle", # Parliament type
-                                     parl_rows = 4,      # Number of rows of the parliament
-                                     party_seats = spain$n)
+spain_semicircle <- parliament_data(
+  election_data = spain,
+  type = "semicircle", # Parliament type
+  parl_rows = 4, # Number of rows of the parliament
+  party_seats = spain$n
+)
 
 
 spain_plot <- ggplot(spain_semicircle, aes(x = x, y = y, colour = voted)) +
   geom_parliament_seats(size = 4) +
-  geom_text(x =0, y = 0.2, label = "Spain", color = "white", size=5, family = "Henny Penny") +
+  geom_text(x = 0, y = 0.2, label = "Spain", color = "white", size = 5, family = "Henny Penny") +
   theme_ggparliament(legend = "none") +
   scale_color_manual(values = base_color)
 
 
-uk <- df %>% 
-  filter(to_country == "United Kingdom",
-         from_country != "United Kingdom") %>% 
+uk <- df %>%
+  filter(
+    to_country == "United Kingdom",
+    from_country != "United Kingdom"
+  ) %>%
   count(voted)
 
-uk_semicircle <- parliament_data(election_data = uk,
-                                    type = "semicircle", # Parliament type
-                                    parl_rows = 4,      # Number of rows of the parliament
-                                    party_seats = uk$n)
+uk_semicircle <- parliament_data(
+  election_data = uk,
+  type = "semicircle", # Parliament type
+  parl_rows = 4, # Number of rows of the parliament
+  party_seats = uk$n
+)
 
 
 uk_plot <- ggplot(uk_semicircle, aes(x = x, y = y, colour = voted)) +
   geom_parliament_seats(size = 4) +
-  geom_text(x =0, y = 0.2, label = "UK", color = "white", size=5, family = "Henny Penny") +
+  geom_text(x = 0, y = 0.2, label = "UK", color = "white", size = 5, family = "Henny Penny") +
   theme_ggparliament(legend = "none") +
   scale_color_manual(values = base_color)
 
 
 # Main plot
 (germany_plot + uk_plot) / (spain_plot + france_plot) +
-  plot_annotation(title = 'Eurovision',
-                  subtitle ="Number of countries that have <b style='color:#fd8d3c'>never awarded points</b> or have <b style='color:#49006a'>awarded points</b> to<br /> the Big Four in the Eurovision contest (1975-2022). Only viewers votes.",
-                  caption = caption,
-                  theme= theme(plot.title  = element_markdown(family = "Henny Penny",
-                                                          colour = "white",
-                                                   size = 25),
-                               plot.subtitle = element_markdown(family ="Luminari",
-                                                                size =11,
-                                                                hjust = 0,
-                                                                margin = margin(5,0,15,0),
-                                                                lineheight = 0.1),
-                               plot.caption = element_markdown(size = 5))) &
-  theme(text = element_text(color = "white"),
-        plot.background = element_rect(color = "black",
-                                        fill = "black"),
-panel.background = element_rect(color = "black",
-                              fill = "black"))
+  plot_annotation(
+    title = "Eurovision",
+    subtitle = "Number of countries that have <b style='color:#fd8d3c'>never awarded points</b> or have <b style='color:#49006a'>awarded points</b> to<br /> the Big Four in the Eurovision contest (1975-2022). Only viewers votes.",
+    caption = caption,
+    theme = theme(
+      plot.title = element_markdown(
+        family = "Henny Penny",
+        colour = "white",
+        size = 25
+      ),
+      plot.subtitle = element_markdown(
+        family = "Luminari",
+        size = 11,
+        hjust = 0,
+        margin = margin(5, 0, 15, 0),
+        lineheight = 0.1
+      ),
+      plot.caption = element_markdown(size = 5)
+    )
+  ) &
+  theme(
+    text = element_text(color = "white"),
+    plot.background = element_rect(
+      color = "black",
+      fill = "black"
+    ),
+    panel.background = element_rect(color = "black", fill = "black")
+  )
 
 ggsave(filename = "2024/plots/eurovision.png", width = 6, height = 5)
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 30DayChartChallenge
+
+Collection of contributions to the [#30DayChartChallenge](https://github.com/30DayChartChallenge/Edition2024?tab=readme-ov-file)
+
+## 2024
+
+Day 1: Part-to-Whole
+
+Germany, the UK, France and Spain are known as the "Big four" in the Eurovision Song Contest. However, they have never been voted for by the public in some countries.
+
+<img src="2024/plots/eurovision.png" width="60%" height="30%"/>
